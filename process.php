a2a60e5fcf1ab6342afed8f075fd9eb07974b4f6
Panayot Angarev <pancho.angarev@gmail.com>
2017-02-15 10:08:17 +0200

    Addd errors array

--- process.php
+++ process.php
@@ -3,33 +3,42 @@
 $myname = $_REQUEST['myname'];
 $mypassword = $_REQUEST['mypassword'];
 $mypasswordconf = $_REQUEST['mypasswordconf'];
+$errors = array();
 
 
 if ($myname === ""):
 
-	echo "<div>Sorry, your name is required field.</div>";
+	$errors[] = "<div>Sorry, your name is required field.</div>";
 
 endif;//input empty field
 
 
 if (strlen($mypassword) <= 6):
 
-	echo "<div>Sorry, the password must be at least six characters.</div>";
+	$errors[] = "<div>Sorry, the password must be at least six characters.</div>";
 
 endif; // password not long enough
 
 if ($mypassword !== $mypasswordconf) :
 	
-	echo "<div>Sorry, password must match.</div>";
+	$errors[] = "<div>Sorry, password must match.</div>";
 
 endif; // password don't match
 
 
 if (!(preg_match('/[A-Za-z]+, [A-Za-z]+/', $myname))):
 
-	echo "<div>Sorry, the name must be in the format: Last name, First name.</div>";
+	$errors[] = "<div>Sorry, the name must be in the format: Last name, First name.</div>";
 
 endif; //pattern doesn't match
+
+if ( !empty($errors))	 :
+
+	foreach ($errors as $error) {
+		echo $error . "<br/>";
+	}
+
+endif;
 
 
 ?>
